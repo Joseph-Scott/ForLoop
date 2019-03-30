05bc0449ece8aed15b50b483c00858c0cfec59ba
Joseph Scott <jbs198585@gmail.com>
2019-03-30 18:49:38 -0400

    challenge question

---
+++ src/com/joescott/Main.java
@@ -0,0 +1,24 @@
+package com.joescott;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+        System.out.println("10,000 at 3% interest = " + calculateInterest(10000.0, 3.0));
+        System.out.println("10,000 at 4% interest = " + calculateInterest(10000.0, 4.0));
+        System.out.println("10,000 at 5% interest = " + calculateInterest(10000.0, 5.0));
+
+        for(int i=0; i<5; i++) {
+            System.out.println("Loop " + i + " hello!");
+        }
+
+        // using the for statement, call the calculateInterest method with
+        // the amount of 10000 with an interestRate of 2,3,4,5,6,7, and 8
+        // and print the results to the console window.
+
+    }
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return(amount * (interestRate / 100));
+    }
+}
