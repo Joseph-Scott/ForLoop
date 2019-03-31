299cc5b4ec8e18de8a8e9bade71b6178f8e39b12
Joseph Scott <jbs198585@gmail.com>
2019-03-30 20:12:13 -0400

    final challenge and solution

--- src/com/joescott/Main.java
+++ src/com/joescott/Main.java
@@ -27,6 +27,42 @@ public class Main {
             System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000.0, i));
         }
 
+        int count = 0;
+        for(int i=10; i<50; i++) {
+            if(isPrime(i)) {
+                count++;
+                System.out.println("Number " + i + " is a prime number");
+                if(count == 3) {
+                    System.out.println("Exiting for loop");
+                    break;
+                }
+            }
+        }
+
+    }
+
+    // Create a for statement using any range of numbers
+    // Determine if the number is a prime number using the isPrime method
+    // if it is a prime number, print it out AND increment a count of the
+    // number of prime numbers found
+    // if that count is 3 exit the for loop
+    // hint: Use the break; statement to exit
+
+    public static boolean isPrime(int n) {
+
+        if(n == 1) {
+            return false;
+        }
+
+        for(int i=2; i <= n/2; i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+
+
     }
 
     public static double calculateInterest(double amount, double interestRate) {
